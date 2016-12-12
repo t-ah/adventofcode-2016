1800ed604c067dfc66fec7674112e4a892195ace
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2016-12-12 16:42:44 +0100

    Add optimization to day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,3 +1,6 @@
+# 2016/12/12: Added some optimization to the visited() method to implicitly check for equivalent states now
+# running in kind of good time now (I'm satisfied at least)
+
 # 1) kind of BFS today.
 # 2) Part Two very slow, I'm sorry.
 # 2a) coincidentally(?), moving the additional parts to floor 4 and moving the elevator
@@ -6,6 +9,8 @@
 
 import re, copy
 from itertools import product
+
+partTwo = False
 
 patGen = re.compile(r" (\S+) generator")
 patChip = re.compile(r" (\S+)-compatible")
@@ -17,16 +22,36 @@ nodes = [[0, items]]
 floor = 0
 itemCount = 0
 
-lookup = {}
 visitList = {}
 
 # check if node was already visisted, if not, treat it as visited from now on
 def visited(floor, nodeItems):
+    lookup = {}
     code = str(floor)
+
+    gens = {}
+    # prepare optimization
+    for floorNo in range(4):
+        for item in nodeItems[floorNo]:
+            if item[0] == "g":
+                gens[item[1]] = str(floorNo)
+
     for floor in range(4):
-        code += str(floor)
-        nodeItems[floor].sort()
-        code += "".join(nodeItems[floor])
+        code += ":"
+
+        #nodeItems[floor].sort()
+        # first optimization attempt for historic reasons
+        #code += "".join([x[:1] + getAlias(x[1:], lookup) for x in nodeItems[floor]]) # catches a few equivalent states (but not many)
+
+        # try some weird code now (only store chips pointing to their gen, sort chips)
+        chips = []
+        for item in nodeItems[floor]:
+            if item[0] == "c":
+                chips.append(gens[item[1]]) # append floorNo of the corresp. generator
+        chips.sort()
+        code += "".join( chips )
+        # oh daym it worked
+
     if code in visitList:
         return True
     else:
@@ -34,17 +59,19 @@ def visited(floor, nodeItems):
         return False
 
 # get a shorter alias for the string (optimize string lengths a bit)
-def getAlias(name):
+def getAlias(name, lookup):
     if not name in lookup:
         lookup[name] = str(len(lookup))
     return lookup[name]
 
 # parse input
 with open("input.txt", "r") as f:
-    # items[0] += ["ga", "ca", "gb", "cb"] # enable this line for part two (kind of bad running time)
+    if partTwo:
+        items[0] += ["ga", "ca", "gb", "cb"] # enable this line for part two
+    lookup = {}
     for line in f:
-        items[floor] += ["g" + getAlias(x) for x in patGen.findall(line)]
-        items[floor] +=  ["c" + getAlias(x) for x in patChip.findall(line)]
+        items[floor] += ["g" + getAlias(x, lookup) for x in patGen.findall(line)]
+        items[floor] +=  ["c" + getAlias(x, lookup) for x in patChip.findall(line)]
         itemCount += len(items[floor])
         floor += 1
 
